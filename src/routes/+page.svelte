14ffc35c0f5c6816d219c05cc737f9cf602789c2
Rahaf <rahafayeshalharbi@gmail.com>
2024-06-23 19:53:36 +0300

    centerd white cyrcle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+
+<title>
+    calculator
+</title>
+
+</svelte:head>
+
+    <div  class="bg-white h-[30rem] w-[20rem]  rounded-3xl  flex justify-center   items-center"> 
+
+    <div class ="bg-pink-800    h-[6rem] w-[6rem] rounded-full  ">
+
+    </div>
+
+    </div>
+
