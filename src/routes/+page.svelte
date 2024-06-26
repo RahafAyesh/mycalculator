843348cf5cd8d91d23244ea7658467d4d0a63494
RahafAyesh <rahafayeshalharbi@gmail.com>
2024-06-26 08:42:27 +0300

    everything is working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,10 +12,19 @@ equation+=value;
 }
 
 function backspace(){
-eqution=equation.substring(0, eqution.length - 1);
+    equation=equation.substring(0, equation.length - 1);
+
+}
+
+function clear(){
+
+    equation=("");
 
 }
  
+function result(){
+equation=eval(equation);
+}
 
 let equation :string="";
 
@@ -42,36 +51,34 @@ let equation :string="";
 <Backspace/>
 
 </button>
-<button class="bg-[#f7d9e4]  hover:bg-[#f2ccda]">CE </button>
-<button class="bg-[#b5d9f7] hover:bg-[#a8cfef] text-white">C </button>
+<button on:click= {clear} class="bg-[#f7d9e4]  hover:bg-[#f2ccda] ">C </button>
+<button on:click= {()=>addtoEquation('-')} class="bg-[#b5d9f7] hover:bg-[#a8cfef] text-white"> <Minus/></button>
 <button on:click={()=>addtoEquation('7')}>7</button>
 <button on:click={()=>addtoEquation('8')}>8</button>
 <button on:click={()=>addtoEquation('9')}>9</button>
-<button  on:click={()=>addtoEquation(' - ')} class="bg-[#c1f1a5] hover:bg-[#b3eb95] text-white">
-<Minus/>
+<button  on:click={()=>addtoEquation('/')} class="bg-[#c1f1a5] hover:bg-[#b3eb95] text-white">
+<Division/>
 </button>
 <button on:click={()=>addtoEquation('4')}>4</button>
 <button on:click={()=>addtoEquation('5')}>5</button>
 <button on:click={()=>addtoEquation('6')}>6</button>
-<button  on:click={()=>addtoEquation('/')} class="bg-[#afe690] hover:bg-[#9dd27f] text-white"> 
-<Division/>
+<button  on:click={()=>addtoEquation('*')} class="bg-[#afe690] hover:bg-[#9dd27f] text-white"> 
+    <Multiplication />
 </button>
 <button on:click={()=>addtoEquation('1')}>1</button>
 <button on:click={() => addtoEquation('2')}>2</button>
 <button on:click={() => addtoEquation('3')}>3</button>
-<button on:click={() => addtoEquation(' * ')} class="bg-[#faaac7] hover:bg-[#ee9cba] text-white">
-    <Multiplication />
+<button on:click={() => addtoEquation('+')} class="bg-[#faaac7] hover:bg-[#ee9cba] text-white">
+    <Addtion/>
 </button>
 
 <button on:click={()=>addtoEquation('.')}>.</button>
 <button on:click={()=>addtoEquation('0')}>0</button>
-<button class="bg-[#f7d9e4] hover:bg-[#e1becb] ">
+<button on:click= {()=> result()} class="col-span-2 bg-[#ffc8dd] hover:bg-[#f0b7cd] ">
 <Equal/>
 </button>
-<button on:click={()=>addtoEquation(' + ')} class="bg-[#ffc8dd] hover:bg-[#f0b7cd] text-white ">
-   <Addtion/>
 
-</button>
+
 
     </div>
 
